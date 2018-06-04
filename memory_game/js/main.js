42e573c8fa73aa85300214a84af84d1e8f7c1cbb
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-06-04 11:21:38 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards =["queen","queen","king","king"];
+var cards =[
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -9,9 +31,11 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 	}
 }
 var flipCard = function(cardID) {
-	console.log("User flipped" + " " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped" + " " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
 	checkForMatch();
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 }
 flipCard(0);
 flipCard(2);
